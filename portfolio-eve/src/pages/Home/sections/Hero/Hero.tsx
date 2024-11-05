6a7811d5758b1eec37a0d7ce9434dea3a40f2b12
Évelim Dornelles <dorrrnelles@gmail.com>
2024-11-05 13:02:57 -0300

    upload

--- portfolio-eve/src/pages/Home/sections/Hero/Hero.tsx
+++ portfolio-eve/src/pages/Home/sections/Hero/Hero.tsx
@@ -1,13 +1,23 @@
 import { styled } from "@mui/material"
+import Avatar from "../../../../assets/images/avatar.jpg"
 
 const Hero = () => {
     const StyledHero = styled("div")(() => ({
         backgroundColor: "Black"
     }))
 
+    const StyledImage = styled("img")(() => ({
+        width: "30%",
+        borderRadius: "50%",
+    }))
+
     return (
         <>
-            <StyledHero>Ola</StyledHero>
+            <StyledHero>Ola
+
+                <StyledImage src={Avatar} />
+
+            </StyledHero>
         </>
     )
 }
